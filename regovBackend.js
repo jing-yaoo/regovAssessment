e856624e046dca38812c362d1992a36be7f9a2ab
hackerman <jingyao990@gmail.com>
2023-07-31 18:52:33 +0200

    fix: Fixed connection configuration.
    Changed "host:" and "port:" in mysql.createConnection

--- regovBackend.js
+++ regovBackend.js
@@ -1,16 +1,16 @@
-// importing necessary modules
+// Importing necessary modules
 const express = require('express'); 
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
 
-// setup mysql connection configuration
+// Setup mysql connection configuration
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_mysql_username',
-  password: 'your_mysql_password',
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'password',
   database: 'user_registration',
   port: '3306',
 });
@@ -24,7 +24,7 @@ db.connect((err) => {
   console.log('Connected to MySQL database.');
 });
 
-//
+// Set parsing for JSON data requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -46,13 +46,16 @@ app.post('/register', (req, res) => {
 // Login function
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
+  // Selecting users which match the credentials
   const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
   db.query(sql, [username, password], (err, result) => {
+    // Error handling and status response
     if (err) {
       console.error('Error logging in: ', err);
       res.status(500).send('Error logging in.');
       return;
     }
+    // Returns error if the password section has no input
     if (result.length === 0) {
       res.status(401).send('Invalid username or password.');
       return;
@@ -60,6 +63,12 @@ app.post('/login', (req, res) => {
     res.status(200).send('Login successful.');
   });
 });
+
+// // Logout function
+// app.post('/logout', (req, res)) => {
+//   const { username, password } = req.body;
+// });
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
